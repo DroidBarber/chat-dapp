9fc8043beaa181f1ae8be85e4b3ea1fcc44f92cc
Mehdi Barim <60230244+DroidBarber@users.noreply.github.com>
2021-09-01 22:26:14 +0200

    first commit

---
+++ src/user.js
@@ -0,0 +1,23 @@
+import GUN from 'gun';
+import 'gun/sea';
+import 'gun/axe';
+import { writable } from 'svelte/store';
+
+//db
+
+export const db = GUN();
+
+//user
+export const user = db.user().recall({sessionStorage: true});
+
+//current user username
+export const usernale = writable('');
+
+user.get('alias').on(v => username.set(v))
+
+db.on('auth', async(event) => {
+    const alias = await user.get('alias'); // username string
+    username.set(alias);
+
+    console.log(`signed in as ${alias}`);
+});
